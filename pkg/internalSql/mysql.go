4e2a2df3dade66e76524d730dcaf02674339730d
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-05-21 22:16:13 -0300

    feat(db): add mysql connection helper

---
+++ pkg/internalSql/mysql.go
@@ -0,0 +1,23 @@
+package internalsql
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectMySQL(dbUser, dbPassword, dbHost, dbPort, dbName string) (*sql.DB, error) {
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&loc=%s", dbUser, dbPassword, dbHost, dbPort, dbName, "America%2FSao_Paulo")
+
+	db, err := sql.Open("mysql", dataSourceName)
+
+	if err != nil {
+		return nil, fmt.Errorf("error connecting to database")
+	}
+
+	log.Println("database connected")
+
+	return db, err
+}
