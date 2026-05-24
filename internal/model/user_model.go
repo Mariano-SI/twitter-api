80373e04c281ad0de6e329dd9cab7921ca813486
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-05-24 16:21:48 -0300

    feat(model): add NewUserModel constructor with bcrypt and uuid

--- internal/model/user_model.go
+++ internal/model/user_model.go
@@ -1,6 +1,11 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type UserModel struct {
 	ID        string
@@ -9,4 +14,21 @@ type UserModel struct {
 	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewUserModel(email, username, password string) (*UserModel, error) {
+	hashed, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	now := time.Now()
+	return &UserModel{
+		ID:        uuid.NewString(),
+		Email:     email,
+		Username:  username,
+		Password:  string(hashed),
+		CreatedAt: now,
+		UpdatedAt: now,
+	}, nil
 }
