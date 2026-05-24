211d6ffc01122e90712b1f12fa8e74971b63902a
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-05-24 16:21:37 -0300

    fix(dto): correct PasswordConfirm field typo

--- internal/dto/user_dto.go
+++ internal/dto/user_dto.go
@@ -4,7 +4,7 @@ type RegisterUserDto struct{
 	Email     string `json:"email"`
 	Username  string `json:"username"`
 	Password  string `json:"password"`
-	PassworConfirm string `json:"password_confirm"`
+	PasswordConfirm string `json:"password_confirm"`
 }
 
 type RegisterUserResponseDto struct{
