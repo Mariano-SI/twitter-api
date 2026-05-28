ed9fac9d5ffc783fc7e72eb50a650274212b4a20
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-05-27 22:49:21 -0300

    feat(jwt): add token validation with optional expiry check

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,14 +1,15 @@
 package jwt
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-func CreateToken(id, username, secretKey string) (string, error) {
+func CreateToken(userId, username, secretKey string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":       id,
+		"id":       userId,
 		"username": username,
 		"exp":      time.Now().Add(60 * time.Minute).Unix(),
 	})
@@ -18,4 +19,35 @@ func CreateToken(id, username, secretKey string) (string, error) {
 	tokenStr, err := token.SignedString(key)
 
 	return tokenStr, err
+}
+
+func ValidateToken(tokenStr, secretKey string, withClaimValidation bool) (string, string, error) {
+	var (
+		key    = []byte(secretKey)
+		claims = jwt.MapClaims{}
+		token  *jwt.Token
+		err    error
+	)
+
+	if withClaimValidation {
+		token, err = jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) {
+			return key, nil
+		})
+	} else {
+		token, err = jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) {
+			return key, nil
+		}, jwt.WithoutClaimsValidation())
+	}
+
+	if err != nil {
+		return "", "", err
+	}
+
+	if !token.Valid {
+		return "", "", errors.New("invalid token")
+	}
+
+	userId, userName := claims["id"].(string), claims["username"].(string)
+
+	return userId, userName, nil
 }
