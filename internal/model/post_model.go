880ed4466e4bfefcb1d50b9e271b165d6c27fcb6
Mariano-SI <mariano.silva@ufvjm.edu.br>
2026-05-28 21:34:01 -0300

    feat: post dtos

---
+++ internal/model/post_model.go
@@ -0,0 +1,27 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type PostModel struct {
+	ID        string
+	Content   string
+	UserId    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time
+}
+
+func NewPostModel(userId, content string) (*PostModel, error) {
+	now := time.Now()
+	return &PostModel{
+		ID:        uuid.NewString(),
+		Content:   content,
+		UserId:    userId,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}, nil
+}
